95a127d3a132a13491831eba79cf75a2d66d3706
Jenna Hakki <jenna@Jennas-MacBook-Pro.local>
2017-12-10 09:43:47 -0500

    Segues Programmatically

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class LeagueVC: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onTappedNext(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
+    
+    
     
 
     /*
